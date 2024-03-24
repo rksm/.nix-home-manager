112bbfb69a57da163feb0c386548233e811b992b
debian <debian@debian-nix.debian-nix>
2024-03-24 11:30:42 -0700

    multiple cursors

--- home.nix
+++ home.nix
@@ -24,6 +24,10 @@
       rustic
       toml-mode
       direnv
+      which-key
+      expand-region
+      key-chord
+      multiple-cursors
     ];
   };
 
@@ -54,6 +58,7 @@
 (direnv-mode)
 (global-company-mode)
 (global-set-key (kbd "M-m") 'company-complete)
+(which-key-mode)
 (vertico-mode)
 (require 'orderless)
 (setq completion-styles '(orderless basic)
@@ -73,7 +78,68 @@
   (setq-local buffer-save-without-query t))
 
 (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook)
-  '';
+
+;; ---------------------
+
+(defun rk/move-beginning-of-line (arg)
+  "move either at `bol' or when already there then in front of
+  the first non-whitespace char"
+  (interactive "p")
+  (let ((p (point)))
+    (back-to-indentation)
+    (when (= p (point))
+      (move-beginning-of-line arg))))
+
+(global-set-key (kbd "C-a") 'rk/move-beginning-of-line)
+
+(key-chord-mode 1)
+(key-chord-define-global "vm" 'er/expand-region)
+(key-chord-define-global "dh" 'er/contract-region)
+
+;; --------------------
+
+(defun rk/mc-cursors-merge ()
+  "Removes cursors that are at the same position"
+  (interactive)
+  (let ((cursors (seq-sort-by (lambda (o) (overlay-start o)) '< (mc/all-fake-cursors))))
+    (while (= (point) (overlay-start (car cursors)))
+      (mc/remove-fake-cursor (car cursors))
+      (setq cursors (cdr cursors)))
+    (let ((last-cursor (car cursors)))
+      (dolist (cursor (cdr cursors))
+	(let ((pos (overlay-start cursor)))
+	  (if (or (= (point) pos) (= (overlay-start last-cursor) pos))
+	      (mc/remove-fake-cursor cursor)
+	    (setq last-cursor cursor)))))
+    (mc/maybe-multiple-cursors-mode)))
+
+(require 'cl-lib)
+(defun rk/mc-mark-next-like-this-and-cycle-forward (arg)
+  "Like `mc/mark-next-like-this' but also forward-cycles the cursor."
+  (interactive "p")
+  (cl-flet ((recenter () nil))
+   (when (< arg 0) (mc/cycle-backward))
+   (mc/mark-next-like-this arg)
+   (rk/mc-cursors-merge)
+   (when (>= arg 0) (mc/cycle-forward))))
+
+(defun rk/mc-mark-previous-like-this-and-cycle-backward (arg)
+  "Like `mc/mark-previous-like-this' but also backward-cycles the cursor."
+  (interactive "p")
+  (cl-flet ((recenter () nil))
+   (when (< arg 0) (mc/cycle-forward))
+   (mc/mark-previous-like-this arg)
+   (rk/mc-cursors-merge)
+   (when (>= arg 0) (mc/cycle-backward))))
+
+(global-set-key (kbd "C-S-<") 'rk/mc-mark-previous-like-this-and-cycle-backward)
+(global-set-key (kbd "C-c <") 'rk/mc-mark-previous-like-this-and-cycle-backward)
+(global-set-key (kbd "C-S->") 'rk/mc-mark-next-like-this-and-cycle-forward)
+(global-set-key (kbd "C-c >") 'rk/mc-mark-next-like-this-and-cycle-forward)
+(global-set-key (kbd "C-M-n") 'mc/mark-next-lines)
+(global-set-key (kbd "C-M-p") 'mc/mark-previous-lines)
+(global-set-key (kbd "C-x R") 'mc/edit-lines)
+'';
 
   home.packages = with pkgs; [
     nil # nix lsp
